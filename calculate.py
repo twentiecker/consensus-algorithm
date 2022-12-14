e7bb570bf5f749f940701ce4d4fa187704fe7a4f
twentiecker <twentiecker@gmail.com>
2022-12-14 21:20:12 +0700

    add comment

--- calculate.py
+++ calculate.py
@@ -6,45 +6,47 @@ class Calculate:
     def consensus(self, spn_tree):
         def average(list_input):
             n = len(list_input)
-            xs = 0
-            for xi in list_input:
-                xs += xi
-            avg = xs / n
+            total = 0
+            for val in list_input:
+                total += val
+            avg = total / n
             return avg
 
         input_threshold = input("Masukkan threshold penghitungan konsensus: ")
         threshold = float(input_threshold)
-        dict_iter = {}
+        dict_iterasi = {}
         list_temp = []
 
         spn_tree1 = spn_tree
-        print(f"spn_tree.nodes: {spn_tree.nodes}")  # mendapatkan nodes
+        print(f"spn_tree.nodes: {spn_tree.nodes}")  # mendapatkan seluruh node
         for u in spn_tree.nodes:
-            list_temp.append(spn_tree.nodes[u]['sval'])  # sval untuk mendapatkan nilai skalar yang disimpan
+            list_temp.append(spn_tree.nodes[u]['sval'])  # sval untuk mendapatkan nilai skalar tiap node
         print(f"list_temp: {list_temp}")
         self.list_consensus.append(list_temp)
         while True:
             list_avg = []
             for u in spn_tree.nodes:
                 x = spn_tree.nodes[u]['sval']
-                dict_iter[u] = [x]
+                dict_iterasi[u] = [x]
                 for v in spn_tree.nodes:
                     if spn_tree.has_edge(u, v):
                         y = spn_tree.nodes[v]['sval']
-                        dict_iter[u].append(y)
-            print(f"dict_iter: {dict_iter}")
+                        dict_iterasi[u].append(y)
+            print(f"dict_iterasi: {dict_iterasi}")
             for u in spn_tree.nodes:
-                avgu = average(dict_iter[u])
-                dict_iter[u] = avgu
-                list_avg.append(avgu)
-                spn_tree.nodes[u]['sval'] = avgu
+                avg_u = average(dict_iterasi[u])
+                dict_iterasi[u] = avg_u
+                list_avg.append(avg_u)
+                spn_tree.nodes[u]['sval'] = avg_u
             self.list_consensus.append(list_avg)
+
+            # Memeriksa apakah selisih nilai max dan min sudah mencapai threshold
             if ((max(list_avg)) - (min(list_avg))) <= threshold:
                 break
             self.iterasi += 1
         print(f"\nKonsensus pada iterasi ke-{self.iterasi}, dengan nilai Skalar tiap node sebagai berikut:")
         list_sf = []
         for i in spn_tree1.nodes:
-            print(f"{i}: {dict_iter[i]}")
-            list_sf.append(dict_iter[i])
+            print(f"{i}: {dict_iterasi[i]}")
+            list_sf.append(dict_iterasi[i])
         print(f"\nRata-rata nilai Skalar semua node = {average(list_sf)}")
